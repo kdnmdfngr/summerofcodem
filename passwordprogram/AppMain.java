cd5fd6c0e13b63da1c76f2ebda17762faba285b5
kidnamedfinger <ariadne.waterra@outlook.com>
2026-06-26 22:27:09 -0700

    whitespace changes to increase readability :)))

--- passwordprogram/AppMain.java
+++ passwordprogram/AppMain.java
@@ -8,40 +8,40 @@
 */
 
 import java.util.*;
+
 public class AppMain {
     public static void main(String[] args) {
 
-    //greet
-    System.out.println("what is goody my gang");
+        // greet
+        System.out.println("what is goody my gang");
 
-    //password check!
-    //password prompt first
-    System.out.println("gimme the magic word. the single one you can describe EVERYTHING WITH");
+        // password check!
+        // password prompt first
+        System.out.println("gimme the magic word. the single one you can describe EVERYTHING WITH");
         Scanner console = new Scanner(System.in);
         String magicWord = "gnarly";
 
-    //read the password
-    String guess = console.nextLine();
-    
-    while(!guess.equals(magicWord)) {
-System.out.println("no??? :(");
+        // read the password
+        String guess = console.nextLine();
 
-//recheck
+        while (!guess.equals(magicWord)) {
+            System.out.println("no??? :(");
 
-System.out.println("like- boba tea. tesla in a bad way. fried chicken. party in the hollywood hills");
+            // recheck
 
-//read password
-guess = console.nextLine();
-    }
-    //now password check
-    if (guess.equals(magicWord)) {
-        System.out.println("everything's gnarly :)");
-        System.out.println("yippee!!!");
-    } 
+            System.out.println("like- boba tea. tesla in a bad way. fried chicken. party in the hollywood hills");
 
-    System.out.println("END OF PROGRAM");
-    
-    
+            // read password
+            guess = console.nextLine();
+        }
+        // now password check
+        if (guess.equals(magicWord)) {
+            System.out.println("everything's gnarly :)");
+            System.out.println("yippee!!!");
+        }
+
+        System.out.println("END OF PROGRAM");
+
         console.close();
     }
 }
